aabbb99d20243226afa44d42844c761bc2a1ca8d
monika <monikanaglak@outlook.fr>
2023-02-23 21:16:04 +0100

    making div on the flow

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+/*window.onload = setTimeout(() => {
+  let x = document.querySelector(".hello");
+  let y = document.querySelector(".welcome");
+  let z = document.querySelector(".please");
+  x.classList.add("hidden")
+  y.classList.add("hiddenalso")
+  z.classList.add("visible")
+}, "3000")
+*/
 let city = document.getElementById("city");
 let btn = document.querySelector("#btn");
 let weather = {
   apiKey: "9a947decfc79f5effa7b7d90a7f7093d",
+  fetchWeather:function(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Leszno&appid=9a947decfc79f5effa7b7d90a7f7093d")
+    .then((response)=>response.json())
+    .then((data)=>console.log(data))
+    
+  }
 };
 btn.addEventListener("click", (e) => {
   e.preventDefault();
