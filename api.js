363076cea13987179afbbe55aa4587ef1283eaf2
monika <monikanaglak@outlook.fr>
2023-02-24 21:00:33 +0100

    start style

--- api.js
+++ api.js
@@ -1,20 +1,24 @@
+
 let btn_send = document.querySelector("#btn");
 btn_send.addEventListener("click", (e) => {
   e.preventDefault();
   let input_city = document.querySelector(".city").value;
-  const valeur_string =
+  let samo = document.querySelector(".city");
+  const regex =
     /^(?=.{2,50}$)[[a-zàáâäçèéêëìíîïñòóôöùúûü]+(?:['-.\s][a-z]+)*$/i;
-  if(input_city.match(valeur_string) && input_city.value !== ""){
+  if(input_city.match(regex) && input_city.value !== ""){
     fetchingData(input_city)
+    samo.value="";
   }
   else{
-    console.log("sometnih is wrong")
     document.querySelector(".error").innerHTML = "Your data is not correct"
   }
+  
   
 });
 
 function fetchingData(input_city) {
+  console.log("im here")
   let apiKey = "9a947decfc79f5effa7b7d90a7f7093d";
 
   fetch(
@@ -25,10 +29,12 @@ function fetchingData(input_city) {
   )
     .then((response) => response.json())
     .then((data) => displayData(data));
+  
 }
 
 function displayData(data) {
+ 
   console.log(data);
-  console.log(data.weather[0].description)
+  console.log(data.weather[0].main)
   document.querySelector(".cityy").innerHTML = data.name
 }
