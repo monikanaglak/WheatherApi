e5691d2700893ab2b19ba354a24a7bf51a61c559
monika <monikanaglak@outlook.fr>
2023-02-25 17:00:32 +0100

    test

--- api.js
+++ api.js
@@ -1,24 +1,45 @@
+//animation rotation card
 
-let btn_send = document.querySelector("#btn");
+/*const container = document.querySelector(".container");
+const card = document.querySelector(".card");
+
+
+
+container.addEventListener('mousemove', (e) => {
+  let xAxis = (window.innerWidth / 1.5 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 1 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+//animate in
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  
+});
+//mouse leave
+
+container.addEventListener("mouseleave", (e) => {
+  card.style.transitions = "all 1s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+});
+*/
 btn_send.addEventListener("click", (e) => {
   e.preventDefault();
   let input_city = document.querySelector(".city").value;
   let samo = document.querySelector(".city");
   const regex =
     /^(?=.{2,50}$)[[a-zàáâäçèéêëìíîïñòóôöùúûü]+(?:['-.\s][a-z]+)*$/i;
-  if(input_city.match(regex) && input_city.value !== ""){
-    fetchingData(input_city)
-    samo.value="";
+  if (input_city.match(regex) && input_city.value !== "") {
+    fetchingData(input_city);
+    samo.value = "";
+  } else {
+    document.querySelector(".error").innerHTML = "Your data is not correct";
   }
-  else{
-    document.querySelector(".error").innerHTML = "Your data is not correct"
-  }
-  
-  
 });
 
 function fetchingData(input_city) {
-  console.log("im here")
+  console.log("im here");
   let apiKey = "9a947decfc79f5effa7b7d90a7f7093d";
 
   fetch(
@@ -29,12 +50,10 @@ function fetchingData(input_city) {
   )
     .then((response) => response.json())
     .then((data) => displayData(data));
-  
 }
 
 function displayData(data) {
- 
   console.log(data);
-  console.log(data.weather[0].main)
-  document.querySelector(".cityy").innerHTML = data.name
+  console.log(data.weather[0].main);
+  document.querySelector(".cityy").innerHTML = data.name;
 }
