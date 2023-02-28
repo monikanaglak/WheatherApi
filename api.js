19412cf7a82d47ae387f040ddee26006af83a88f
monika <monikanaglak@outlook.fr>
2023-02-28 17:09:00 +0100

    small gsap

--- api.js
+++ api.js
@@ -1,20 +1,19 @@
 const btn_send = document.querySelector("#btn")
-btn_send.addEventListener("click", (e) => {
+  btn_send.addEventListener("click", (e) => {
   e.preventDefault();
   let input_city = document.querySelector(".city").value;
-  let samo = document.querySelector(".city");
+  let input = document.querySelector(".city");
   const regex =
     /^(?=.{2,50}$)[[a-zàáâäçèéêëìíîïñòóôöùúûü]+(?:['-.\s][a-z]+)*$/i;
   if (input_city.match(regex) && input_city.value !== "") {
     fetchingData(input_city);
-    samo.value = "";
+    input.value = "";
   } else {
-    document.querySelector(".error").innerHTML = "Your data is not correct";
+    document.querySelector(".error").innerHTML = "The name of the city is not correct";
   }
 });
 
 function fetchingData(input_city) {
-  console.log("im here");
   let apiKey = "9a947decfc79f5effa7b7d90a7f7093d";
 
   fetch(
@@ -30,6 +29,16 @@ function fetchingData(input_city) {
 function displayData(data) {
   console.log(data);
   console.log(data.weather[0].main);
+  /*let monika = document.querySelector(".cityy").innerHTML;
+  monika=data.name;*/
+
   document.querySelector(".cityy").innerHTML = data.name;
   document.querySelector(".temperature").innerHTML = Math.round(data.main.temp-273)+"°C";
-}
+
+  let tl = gsap.timeline()
+  let tld = gsap.timeline()
+  /*gsap.fromTo(".cityy",{opacity:0},{opacity:1,duration:1,delay:0.5})*/
+  tl.from(".cityy",{opacity:0, y:-20},{duration:1.5,delay:0.5})
+  tld.from(".temperature",{opacity:0, y:-50},{duration:1.5,delay:3})
+ 
+}  
